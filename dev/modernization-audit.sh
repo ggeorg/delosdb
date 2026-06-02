fa2eb8aeec9c59266cbf042696e28b6c404e78a1
George Georgopoulos <george.georgopoulos@zact.com>
2026-06-02 10:04:02 -0700

    Move tools sources into the Gradle module

--- dev/modernization-audit.sh
+++ dev/modernization-audit.sh
@@ -20,7 +20,7 @@ PRODUCTION_ROOTS=(
   "$ROOT_DIR/delosdb-runner/src/main/java"
   "$ROOT_DIR/delosdb-server/src/main/java"
   "$ROOT_DIR/java/org.apache.derby.shared"
-  "$ROOT_DIR/java/org.apache.derby.tools"
+  "$ROOT_DIR/delosdb-tools/src/main/java"
 )
 
 count_matches() {
