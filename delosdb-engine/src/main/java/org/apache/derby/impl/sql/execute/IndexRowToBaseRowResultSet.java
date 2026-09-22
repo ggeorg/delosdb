b07d3cd352f7c9c1fe5c530bf542d171535c43ef
George Georgopoulos <georgopoulos.georgios@gmail.com>
2026-09-21 21:25:35 -0700

    Add batched Gen2-C3 index-to-base-row fetch experiment

--- delosdb-engine/src/main/java/org/apache/derby/impl/sql/execute/IndexRowToBaseRowResultSet.java
+++ delosdb-engine/src/main/java/org/apache/derby/impl/sql/execute/IndexRowToBaseRowResultSet.java
@@ -37,6 +37,8 @@ import org.apache.derby.iapi.store.access.DynamicCompiledOpenConglomInfo;
 import org.apache.derby.iapi.store.access.StaticCompiledOpenConglomInfo;
 import org.apache.derby.iapi.store.access.TransactionController;
 import org.apache.derby.iapi.store.access.conglomerate.AccessMethodBaseFetchPagePrefetch;
+import org.apache.derby.iapi.store.access.conglomerate.AccessMethodBaseFetchBatch;
+import org.apache.derby.iapi.store.types.StoreDataValue;
 import org.apache.derby.iapi.store.types.StoreRowLocation;
 import org.apache.derby.iapi.types.DataValueDescriptor;
 import org.apache.derby.iapi.types.RowLocation;
@@ -69,9 +71,22 @@ class IndexRowToBaseRowResultSet extends NoPutResultSetImpl
 	private boolean                 closeBaseCCHere;
 	private boolean					forUpdate;
 	private DataValueDescriptor[]	rowArray;
+    private static final String BASE_FETCH_BATCH_PROPERTY =
+            "delosdb.experimental.indexToBaseRowBatchFetch";
+    private static final int BASE_FETCH_BATCH_SIZE =
+            Math.max(2, Integer.getInteger("delosdb.experimental.indexToBaseRowBatchSize", 16));
+
     private AccessMethodBaseFetchPagePrefetch baseFetchPagePrefetch;
     private BulkTableScanResultSet baseFetchBulkSource;
     private StoreRowLocation[] baseFetchPrefetchRowLocations;
+    private AccessMethodBaseFetchBatch baseFetchBatch;
+    private ExecRow[] baseFetchBatchSourceRows;
+    private StoreRowLocation[] baseFetchBatchRowLocations;
+    private StoreDataValue[][] baseFetchBatchDestRows;
+    private boolean[] baseFetchBatchExists;
+    private int baseFetchBatchCount;
+    private int baseFetchBatchPosition;
+    private int baseFetchBatchCurrent = -1;
 
 	// changed a whole bunch
 	RowLocation	baseRowLocation;
@@ -332,6 +347,7 @@ class IndexRowToBaseRowResultSet extends NoPutResultSetImpl
 
 		source.reopenCore();
         clearBaseFetchPagePrefetch();
+        clearBaseFetchBatchState();
 
 		numOpens++;
 		openTime += getElapsedMillis(beginTime);
@@ -372,7 +388,7 @@ class IndexRowToBaseRowResultSet extends NoPutResultSetImpl
 		 */
 		do 
 		{
-			sourceRow = source.getNextRowCore();
+			sourceRow = nextBaseFetchSourceRow();
 
 			if (sourceRow != null) {
 
@@ -387,16 +403,23 @@ class IndexRowToBaseRowResultSet extends NoPutResultSetImpl
 				baseRowLocation = (RowLocation)
 						sourceRow.getColumn(sourceRow.nColumns());
 
-                prefetchBufferedBaseRows();
+                if (baseFetchBatch == null) {
+                    prefetchBufferedBaseRows();
+                }
 
 				// Fetch the columns coming from the heap
-				boolean row_exists = 
-                    baseCC.fetch
-                    (
-                     baseRowLocation,
-                     rowArray,
-                     _includeRowLocation ? _heapColsWithoutRowLocation : accessedHeapCols
-                     );
+                boolean row_exists;
+                if (baseFetchBatchCurrent >= 0) {
+                    row_exists = baseFetchBatchExists[baseFetchBatchCurrent];
+                    if (row_exists) {
+                        copyBatchedHeapRow(baseFetchBatchCurrent);
+                    }
+                } else {
+                    row_exists = baseCC.fetch(
+                            baseRowLocation,
+                            rowArray,
+                            _includeRowLocation ? _heapColsWithoutRowLocation : accessedHeapCols);
+                }
 
                 if (row_exists)
                 {
@@ -408,7 +431,7 @@ class IndexRowToBaseRowResultSet extends NoPutResultSetImpl
 					 * then we will be getting all of the columns anew
 					 * from the index (indexCols == null).
 					 */
-					if (! copiedFromSource)
+					if (! copiedFromSource || baseFetchBatch != null)
 					{
 						copiedFromSource = true;
 
@@ -512,6 +535,12 @@ class IndexRowToBaseRowResultSet extends NoPutResultSetImpl
             baseFetchPagePrefetch = null;
             baseFetchBulkSource = null;
             baseFetchPrefetchRowLocations = null;
+            baseFetchBatch = null;
+            baseFetchBatchSourceRows = null;
+            baseFetchBatchRowLocations = null;
+            baseFetchBatchDestRows = null;
+            baseFetchBatchExists = null;
+            clearBaseFetchBatchState();
 	        source.close();
 
 			super.close();
@@ -523,10 +552,108 @@ class IndexRowToBaseRowResultSet extends NoPutResultSetImpl
 		closeTime += getElapsedMillis(beginTime);
 	}
 
+    private ExecRow nextBaseFetchSourceRow() throws StandardException {
+        if (baseFetchBatch == null) {
+            baseFetchBatchCurrent = -1;
+            return source.getNextRowCore();
+        }
+        if (baseFetchBatchPosition >= baseFetchBatchCount) {
+            loadBaseFetchBatch();
+        }
+        if (baseFetchBatchCount == 0) {
+            baseFetchBatchCurrent = -1;
+            return null;
+        }
+        baseFetchBatchCurrent = baseFetchBatchPosition++;
+        return baseFetchBatchSourceRows[baseFetchBatchCurrent];
+    }
+
+    private void loadBaseFetchBatch() throws StandardException {
+        clearBaseFetchBatchState();
+        for (int index = 0; index < baseFetchBatchSourceRows.length; index++) {
+            ExecRow sourceRow = source.getNextRowCore();
+            if (sourceRow == null) {
+                break;
+            }
+            ExecRow detached = sourceRow.getClone();
+            StoreRowLocation location = (StoreRowLocation)
+                    detached.getColumn(detached.nColumns());
+            baseFetchBatchSourceRows[index] = detached;
+            baseFetchBatchRowLocations[index] = location;
+            baseFetchBatchDestRows[index] = batchDestinationRow(
+                    baseFetchBatchDestRows[index]);
+            baseFetchBatchCount++;
+        }
+        if (baseFetchBatchCount > 0) {
+            baseFetchBatch.fetchBaseRows(
+                    baseFetchBatchRowLocations,
+                    baseFetchBatchDestRows,
+                    _includeRowLocation ? _heapColsWithoutRowLocation : accessedHeapCols,
+                    baseFetchBatchExists,
+                    baseFetchBatchCount);
+        }
+    }
+
+    private StoreDataValue[] batchDestinationRow(StoreDataValue[] reusable) {
+        StoreDataValue[] destination = reusable;
+        if (destination == null || destination.length != rowArray.length) {
+            destination = new StoreDataValue[rowArray.length];
+            for (int index = 0; index < rowArray.length; index++) {
+                if (rowArray[index] != null) {
+                    destination[index] = rowArray[index].getNewNull();
+                }
+            }
+            return destination;
+        }
+        for (StoreDataValue value : destination) {
+            if (value instanceof DataValueDescriptor dvd) {
+                dvd.setToNull();
+            }
+        }
+        return destination;
+    }
+
+    private void copyBatchedHeapRow(int batchIndex) throws StandardException {
+        StoreDataValue[] sourceValues = baseFetchBatchDestRows[batchIndex];
+        for (int index = 0; index < rowArray.length; index++) {
+            if (rowArray[index] == null || sourceValues[index] == null) {
+                continue;
+            }
+            rowArray[index].setValue((DataValueDescriptor) sourceValues[index]);
+        }
+    }
+
+    private void clearBaseFetchBatchState() {
+        baseFetchBatchCount = 0;
+        baseFetchBatchPosition = 0;
+        baseFetchBatchCurrent = -1;
+        if (baseFetchBatchSourceRows != null) {
+            for (int index = 0; index < baseFetchBatchSourceRows.length; index++) {
+                baseFetchBatchSourceRows[index] = null;
+                baseFetchBatchRowLocations[index] = null;
+                baseFetchBatchExists[index] = false;
+            }
+        }
+    }
+
     private void configureBaseFetchPagePrefetch() {
         baseFetchPagePrefetch = null;
         baseFetchBulkSource = null;
         baseFetchPrefetchRowLocations = null;
+        baseFetchBatch = null;
+        clearBaseFetchBatchState();
+        if (!forUpdate
+                && !activation.getResultSetHoldability()
+                && Boolean.getBoolean(BASE_FETCH_BATCH_PROPERTY)
+                && baseCC instanceof AccessMethodBaseFetchBatch batch
+                && batch.baseFetchBatchEnabled()) {
+            baseFetchBatch = batch;
+            baseFetchBatchSourceRows = new ExecRow[BASE_FETCH_BATCH_SIZE];
+            baseFetchBatchRowLocations = new StoreRowLocation[BASE_FETCH_BATCH_SIZE];
+            baseFetchBatchDestRows = new StoreDataValue[BASE_FETCH_BATCH_SIZE][];
+            baseFetchBatchExists = new boolean[BASE_FETCH_BATCH_SIZE];
+            return;
+        }
         if (forUpdate
                 || activation.getResultSetHoldability()
                 || !(source instanceof BulkTableScanResultSet bulkSource)
--- delosdb-storage-mvcc/src/main/java/org/apache/derby/impl/store/access/mvcc/MvccRawStoreConglomerateController.java
+++ delosdb-storage-mvcc/src/main/java/org/apache/derby/impl/store/access/mvcc/MvccRawStoreConglomerateController.java
@@ -16,6 +16,7 @@ import org.apache.derby.iapi.services.io.FormatableBitSet;
 import org.apache.derby.iapi.store.access.ConglomerateController;
 import org.apache.derby.iapi.store.access.SpaceInfo;
 import org.apache.derby.iapi.store.access.conglomerate.AccessMethodBaseFetchPagePrefetch;
+import org.apache.derby.iapi.store.access.conglomerate.AccessMethodBaseFetchBatch;
 import org.apache.derby.iapi.store.access.conglomerate.AccessMethodIndexBuildLifecycle;
 import org.apache.derby.iapi.store.access.conglomerate.AccessMethodReadCommittedUpdateRecheck;
 import org.apache.derby.iapi.store.access.conglomerate.AccessMethodUniqueConstraintLifecycle;
@@ -32,6 +33,7 @@ import org.apache.derby.shared.common.error.StandardException;
 /** Controller for the isolated RawStore-backed MVCC table format. */
 final class MvccRawStoreConglomerateController
         implements ConglomerateController, AccessMethodBaseFetchPagePrefetch,
+                AccessMethodBaseFetchBatch,
                 AccessMethodIndexBuildLifecycle, AccessMethodReadCommittedUpdateRecheck,
                 AccessMethodUniqueConstraintLifecycle {
     private final MvccRawStoreRuntime runtime;
@@ -40,8 +42,8 @@ final class MvccRawStoreConglomerateController
     private final Transaction rawTransaction;
     private static final String BASE_FETCH_PAGE_PREFETCH_PROPERTY =
             "delosdb.experimental.mvccBaseFetchPagePrefetch";
-    private static final String BASE_FETCH_CURRENT_ANCHOR_PROPERTY =
-            "delosdb.experimental.mvccBaseFetchCurrentRowAnchor";
+    private static final String BASE_FETCH_BATCH_PROPERTY =
+            "delosdb.experimental.mvccBaseFetchBatch";
 
     private final boolean forUpdate;
     private final MvccRawStoreRuntime.SnapshotLease statementSnapshotLease;
@@ -113,6 +115,96 @@ final class MvccRawStoreConglomerateController
                 statementSnapshotLease.close();
             }
             transactionManager.closeMe(this);
+        }
+    }
+
+    @Override
+    public boolean baseFetchBatchEnabled() {
+        return !forUpdate
+                && table.gen2A1()
+                && statementSnapshotLease != null
+                && Boolean.getBoolean(BASE_FETCH_BATCH_PROPERTY);
+    }
+
+    @Override
+    public void fetchBaseRows(
+            StoreRowLocation[] rowLocations,
+            StoreDataValue[][] destRows,
+            FormatableBitSet validColumns,
+            boolean[] rowExists,
+            int count) throws StandardException {
+        ensureOpen();
+        if (!baseFetchBatchEnabled()
+                || rowLocations == null
+                || destRows == null
+                || rowExists == null) {
+            throw new IllegalStateException("MVCC base-fetch batch is not available");
+        }
+        int limit = Math.min(
+                count,
+                Math.min(rowLocations.length, Math.min(destRows.length, rowExists.length)));
+        boolean[] attempted = new boolean[limit];
+        MvccRawStoreTransactionContext context =
+                runtime.context(transactionManager, rawTransaction);
+        MvccRawStoreVersionRows.FetchProjection projection = readProjection(validColumns);
+        MvccRawStoreTable.CurrentBaseFetchDecoder decoder =
+                currentBaseFetchDecoder(projection);
+        MvccRawStoreRuntime.TableReadBoundary operationBoundary = enterReadBoundary();
+        try {
+            for (int index = 0; index < limit; index++) {
+                if (attempted[index] || rowLocations[index] == null) {
+                    continue;
+                }
+                MvccRowLocation first = MvccRowLocation.from(rowLocations[index]);
+                if (!first.hasLocatorHint()) {
+                    continue;
+                }
+                long pageNumber = first.locatorPageId();
+                Page page = null;
+                try {
+                    page = readDirectoryContainer().getPage(pageNumber);
+                    if (page == null) {
+                        continue;
+                    }
+                    for (int candidate = index; candidate < limit; candidate++) {
+                        if (attempted[candidate] || rowLocations[candidate] == null) {
+                            continue;
+                        }
+                        MvccRowLocation location =
+                                MvccRowLocation.from(rowLocations[candidate]);
+                        if (!location.hasLocatorHint()
+                                || location.locatorPageId() != pageNumber) {
+                            continue;
+                        }
+                        attempted[candidate] = true;
+                        MvccRawStoreTable.VisibleRow visible =
+                                decoder.readVisibleAtHintedPage(
+                                        location,
+                                        page,
+                                        statementSnapshotSequence,
+                                        context.transactionId(),
+                                        readVersionReader());
+                        if (visible != null) {
+                            location.setWriteVersion(visible.versionId());
+                            StoreValueCopySupport.copyRow(
+                                    visible.values(), destRows[candidate], validColumns);
+                            rowExists[candidate] = true;
+                        }
+                    }
+                } finally {
+                    if (page != null) {
+                        page.unlatch();
+                    }
+                }
+            }
+        } finally {
+            closeOperationReadBoundary(operationBoundary);
+        }
+        for (int index = 0; index < limit; index++) {
+            if (!rowExists[index]) {
+                rowExists[index] = fetch(
+                        rowLocations[index], destRows[index], validColumns);
+            }
         }
     }
 
@@ -226,19 +318,7 @@ final class MvccRawStoreConglomerateController
                 long snapshotSequence = statementSnapshotLease != null
                         ? statementSnapshotSequence
                         : context.snapshotSequence();
-                MvccRawStoreTable.VisibleRow anchored = !forUpdate
-                        && !table.gen2A1()
-                        && prefetchedDirectory == null
-                        && Boolean.getBoolean(BASE_FETCH_CURRENT_ANCHOR_PROPERTY)
-                                ? readAnchoredCurrent(
-                                        location,
-                                        projection,
-                                        context,
-                                        snapshotSequence)
-                                : null;
-                if (anchored != null) {
-                    visible = anchored;
-                } else if (!forUpdate && table.gen2A1() && prefetchedDirectory == null) {
+                if (!forUpdate && table.gen2A1() && prefetchedDirectory == null) {
                     visible = currentBaseFetchDecoder(projection).readVisibleAt(
                             location,
                             snapshotSequence,
@@ -294,33 +374,6 @@ final class MvccRawStoreConglomerateController
             FormatableBitSet validColumns,
             boolean waitForLock) throws StandardException {
         return fetch(loc, destRow, validColumns);
-    }
-
-    private MvccRawStoreTable.VisibleRow readAnchoredCurrent(
-            MvccRowLocation location,
-            MvccRawStoreVersionRows.FetchProjection projection,
-            MvccRawStoreTransactionContext context,
-            long snapshotSequence) throws StandardException {
-        if (context.hasPendingVersion(table, location.rowId())) {
-            return null;
-        }
-        MvccRawStoreRuntime.CurrentRowAnchor anchor =
-                context.currentRowAnchor(table, location.rowId());
-        if (anchor == null || !anchor.visibleTo(snapshotSequence) || anchor.tombstone()) {
-            return null;
-        }
-        MvccRawStoreTable.VersionRecord current =
-                readVersionReader().findAnchoredCurrent(anchor, projection);
-        if (current == null) {
-            context.invalidateCurrentRowAnchor(table, location.rowId(), anchor);
-            return null;
-        }
-        return new MvccRawStoreTable.VisibleRow(
-                location.rowId(),
-                current.versionId(),
-                current.values(),
-                current.handle(),
-                anchor.directoryLocation());
     }
 
     @Override
